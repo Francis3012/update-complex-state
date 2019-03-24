db5c0aac7282271c700a35c774a8fbc0346949b6
SweetPicklee <francisbruceclark1998@gmail.com>
2019-03-24 20:05:01 +0000

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  	constructor(props) {
+		super(props);
+		this.state = {
+			instructors: [
+				{
+					name: 'Tim',
+					hobbies: ['sailing', 'react']
+				}, {
+					name: 'Matt',
+					hobbies: ['math', 'd3']
+				}, {
+					name: 'Colt',
+					hobbies: ['css', 'hiking']
+				}, {
+					name: 'Elie',
+					hobbies: ['music', 'es2015']
+				}
+			]
+		};
+	
+		setTimeout(() => {
+			const randInst = Math.floor(
+				Math.random() * 
+				this.state.instructors.length
+			);
+		
+			const hobbyIndex = Math.floor(
+				Math.random() * 
+				this.state.instructors[randInst].length
+			);
+		
+			const instructors = this.state.instructors.map((inst, i) => {
+				if (i === randInst) {
+					const hobbies = [...inst.hobbies];
+					hobbies.splice(hobbyIndex, 1);
+					return {
+						...inst,
+						hobbies
+					};
+				}
+			
+				return inst;
+			});
+			this.setState({instructors});
+		}, 5000);
+	}
+  
+  	render() {
+		const instructors = this.state.instructors.map((instructor, index) => (
+	  		<li key={index}>
+				<h3>{instructor.name}</h3>
+				<h4>Hobbies: {instructor.hobbies.join(", ")}</h4>
+	  		</li>
+		));
+		return (
+	  		<div className="App">
+				<ul>
+		  			{instructors}
+				</ul>
+	  		</div>
+		);
+  	}
 }
 
 export default App;
